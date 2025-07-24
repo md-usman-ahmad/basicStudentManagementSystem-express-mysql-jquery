b1add128b7c7a191c1d2fecac0224b3366a5c880
Usmaan <Usmaan@usmaan.com>
2025-07-25 01:58:41 +0530

    implemented Github OAuth passport.js without using session.(server pe sara code)

--- server.js
+++ server.js
@@ -11,29 +11,29 @@ require("dotenv").config();
 // console.log("process.env = ",process.env);
 
 const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-// Configure Passport to use Google OAuth
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENTID,
-  clientSecret: process.env.GOOGLE_CLIENTSECRET,
-  callbackURL: "http://localhost:8000/google/callback"
+const GithubStrategy = require('passport-github').Strategy;
+// Configure Passport to use Github OAuth
+passport.use(new GithubStrategy({
+  clientID: process.env.GITHUB_CLIENTID,
+  clientSecret: process.env.GITHUB_CLIENTSECRET,
+  callbackURL: "http://localhost:8000/github/callback"
 }, async function(accessToken, refreshToken, profile, cb){
   console.log("profile =",profile);
   try {
-    const outputFromDB = await dbQuery(`SELECT * FROM users WHERE email = ?`, [
-      profile.emails[0].value,
+    const outputFromDB = await dbQuery(`SELECT * FROM users WHERE username = ?`, [
+      profile.username,
     ]);
     console.log("outputFromDB =",outputFromDB);
 
     if (outputFromDB.length > 0) {  
       cb(null, outputFromDB[0]);
     } else {
-      await dbQuery(`INSERT INTO users (name, email,username,password,createdAt,logincount,isAdmin,provider) VALUES (?, ?, ?, ?, ?, ?, ?,?)`, [
-        profile.displayName, profile.emails[0].value, profile.displayName+"_"+profile.id, "", new Date().toISOString().slice(0,10), 0, "no", profile.provider
+      await dbQuery(`INSERT INTO users (name,username,password,createdAt,logincount,isAdmin,provider) VALUES (?, ?, ?, ?, ?, ?,?)`, [
+        profile.displayName, profile.username, "", new Date().toISOString().slice(0,10), 0, "no", profile.provider
       ]);
-      const outputFromDB = await dbQuery(`SELECT * FROM users WHERE email = ?`, [
-        profile.emails[0].value,
+      const outputFromDB = await dbQuery(`SELECT * FROM users WHERE username = ?`, [
+        profile.username,
       ]);
       cb(null, outputFromDB[0]);
     }
@@ -47,12 +47,12 @@ passport.use(new GoogleStrategy({
 app.use(passport.initialize());
 
 // after clicking on 'continue with Google' button 
-app.get("/google", passport.authenticate('google', {
+app.get("/github", passport.authenticate('github', {
   scope: ['profile', 'email'],
-  prompt: 'select_account'  // force Google to show account picker every time
+  prompt: 'select_account'  // force github to show account picker every time
 }));
 
-app.get("/google/callback",passport.authenticate("google",{
+app.get("/github/callback",passport.authenticate("github",{
   session : false
 }), async function(request,response,next){
     console.log("request.user =",request.user);
@@ -72,6 +72,82 @@ app.get("/google/callback",passport.authenticate("google",{
 //   response.send('callback is working')
   response.redirect("http://localhost:8000/home");
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// // Configure Passport to use Google OAuth
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENTID,
+//   clientSecret: process.env.GOOGLE_CLIENTSECRET,
+//   callbackURL: "http://localhost:8000/google/callback"
+// }, async function(accessToken, refreshToken, profile, cb){
+//   console.log("profile =",profile);
+//   try {
+//     const outputFromDB = await dbQuery(`SELECT * FROM users WHERE email = ?`, [
+//       profile.emails[0].value,
+//     ]);
+//     console.log("outputFromDB =",outputFromDB);
+
+//     if (outputFromDB.length > 0) {  
+//       cb(null, outputFromDB[0]);
+//     } else {
+//       await dbQuery(`INSERT INTO users (name, email,username,password,createdAt,logincount,isAdmin,provider) VALUES (?, ?, ?, ?, ?, ?, ?,?)`, [
+//         profile.displayName, profile.emails[0].value, profile.displayName+"_"+profile.id, "", new Date().toISOString().slice(0,10), 0, "no", profile.provider
+//       ]);
+//       const outputFromDB = await dbQuery(`SELECT * FROM users WHERE email = ?`, [
+//         profile.emails[0].value,
+//       ]);
+//       cb(null, outputFromDB[0]);
+//     }
+
+//   } catch (error) {
+//     cb(error,false);
+//   }
+// }))
+
+// // Intiliazing passport as middleware in our application
+// app.use(passport.initialize());
+
+// // after clicking on 'continue with Google' button 
+// app.get("/google", passport.authenticate('google', {
+//   scope: ['profile', 'email'],
+//   prompt: 'select_account'  // force Google to show account picker every time
+// }));
+
+// app.get("/google/callback",passport.authenticate("google",{
+//   session : false
+// }), async function(request,response,next){
+//     console.log("request.user =",request.user);
+//     const user = request.user;
+//     const token = jwt.sign({ isAdmin : user.isAdmin , currentloggedInUsername : user.username}, constants.SECRET, {
+//       expiresIn: '1h'
+//     })
+
+//   response.cookie('token', token)
+
+// // user login krgya isiliye logincount increment krrhe 
+//     let query = `update users 
+//                 set logincount = ?
+//                 where userId = ?`
+//     let params = [user.logincount+1 , user.userId ];
+//     await dbQuery(query,params);
+// //   response.send('callback is working')
+//   response.redirect("http://localhost:8000/home");
+// })
 
 
 
